1c2aa9c856f7b167d447287a91e0e9099b00f388
Madris1402 <ricmad142@gmail.com>
2025-10-17 20:35:14 -0600

    Hechos y algunas Reglas

--- Prolog/republica.pl
+++ Prolog/republica.pl
@@ -1,19 +1,293 @@
 % Base de Conocimientos de la Republica Mexicana %
 
-esEstado(e).
+% --- HECHOS ---
 
-esCapital(c).
+% Estados
+% estado(Nombre, Extension_km2)
 
-esSuperficie().
+estado(aguascalientes, 5616).
+estado(baja_california, 71450).
+estado(baja_california_sur, 73909).
+estado(campeche, 57507).
+estado(chiapas, 73311).
+estado(chihuahua, 247412).
+estado(cdmx, 1495).
+estado(coahuila, 151595).
+estado(colima, 5627).
+estado(durango, 123364).
+estado(guanajuato, 30607).
+estado(guerrero, 63596).
+estado(hidalgo, 20813).
+estado(jalisco, 78588).
+estado(mexico, 22351).
+estado(michoacan, 58599).
+estado(morelos, 4879).
+estado(nayarit, 27857).
+estado(nuevo_leon, 64156).
+estado(oaxaca, 93757).
+estado(puebla, 34306).
+estado(queretaro, 11699).
+estado(quintana_roo, 44705).
+estado(san_luis_potosi, 61138).
+estado(sinaloa, 57365).
+estado(sonora, 179355).
+estado(tabasco, 24731).
+estado(tamaulipas, 80249).
+estado(tlaxcala, 3997).
+estado(veracruz, 71820).
+estado(yucatan, 39524).
+estado(zacatecas, 75284).
 
-esLimitrofe().
+% Capitales
+% capital(estado, capital)
 
-esPequenio().
+capital(aguascalientes, aguascalientes).
+capital(baja_california, mexicali).
+capital(baja_california_sur, la_paz).
+capital(campeche, san_francisco_de_campeche).
+capital(chiapas, tuxtla_gutierrez).
+capital(chihuahua, chihuahua).
+capital(cdmx, cdmx).
+capital(coahuila, saltillo).
+capital(colima, colima).
+capital(durango, victoria_de_durango).
+capital(guanajuato, guanajuato).
+capital(guerrero, chilpancingo_de_los_bravo).
+capital(hidalgo, pachuca_de_soto).
+capital(jalisco, guadalajara).
+capital(mexico, toluca_de_lerdo).
+capital(michoacan, morelia).
+capital(morelos, cuernavaca).
+capital(nayarit, tepic).
+capital(nuevo_leon, monterrey).
+capital(oaxaca, oaxaca_de_juarez).
+capital(puebla, puebla_de_zaragoza).
+capital(queretaro, santiago_de_queretaro).
+capital(quintana_roo, chetumal).
+capital(san_luis_potosi, san_luis_potosi).
+capital(sinaloa, culiacan_rosales).
+capital(sonora, hermosillo).
+capital(tabasco, villahermosa).
+capital(tamaulipas, ciudad_victoria).
+capital(tlaxcala, tlaxcala_de_xicotencatl).
+capital(veracruz, xalapa_enriquez).
+capital(yucatan, merida).
+capital(zacatecas, zacatecas).
 
-esGrande().
 
-masGrande().
+% Estados Limitrofes
+% limitrofe(Estado1, Estado2)
+
+limitrofe(aguascalientes, jalisco).
+limitrofe(aguascalientes, zacatecas).
+limitrofe(baja_california, baja_california_sur).
+limitrofe(baja_california, sonora).
+limitrofe(campeche, quintana_roo).
+limitrofe(campeche, tabasco).
+limitrofe(campeche, yucatan).
+limitrofe(chiapas, oaxaca).
+limitrofe(chiapas, tabasco).
+limitrofe(chiapas, veracruz).
+limitrofe(chihuahua, coahuila).
+limitrofe(chihuahua, durango).
+limitrofe(chihuahua, sinaloa).
+limitrofe(chihuahua, sonora).
+limitrofe(cdmx, mexico).
+limitrofe(cdmx, morelos).
+limitrofe(coahuila, durango).
+limitrofe(coahuila, nuevo_leon).
+limitrofe(coahuila, san_luis_potosi).
+limitrofe(coahuila, zacatecas).
+limitrofe(colima, jalisco).
+limitrofe(colima, michoacan).
+limitrofe(durango, nayarit).
+limitrofe(durango, sinaloa).
+limitrofe(durango, zacatecas).
+limitrofe(guanajuato, jalisco).
+limitrofe(guanajuato, michoacan).
+limitrofe(guanajuato, queretaro).
+limitrofe(guanajuato, san_luis_potosi).
+limitrofe(guanajuato, zacatecas).
+limitrofe(guerrero, mexico).
+limitrofe(guerrero, michoacan).
+limitrofe(guerrero, morelos).
+limitrofe(guerrero, oaxaca).
+limitrofe(guerrero, puebla).
+limitrofe(hidalgo, mexico).
+limitrofe(hidalgo, puebla).
+limitrofe(hidalgo, queretaro).
+limitrofe(hidalgo, san_luis_potosi).
+limitrofe(hidalgo, tlaxcala).
+limitrofe(hidalgo, veracruz).
+limitrofe(jalisco, michoacan).
+limitrofe(jalisco, nayarit).
+limitrofe(jalisco, san_luis_potosi).
+limitrofe(jalisco, zacatecas).
+limitrofe(mexico, michoacan).
+limitrofe(mexico, morelos).
+limitrofe(mexico, puebla).
+limitrofe(mexico, queretaro).
+limitrofe(mexico, tlaxcala).
+limitrofe(michoacan, queretaro).
+limitrofe(morelos, puebla).
+limitrofe(nayarit, sinaloa).
+limitrofe(nayarit, zacatecas).
+limitrofe(nuevo_leon, san_luis_potosi).
+limitrofe(nuevo_leon, tamaulipas).
+limitrofe(nuevo_leon, zacatecas).
+limitrofe(oaxaca, puebla).
+limitrofe(oaxaca, veracruz).
+limitrofe(puebla, tlaxcala).
+limitrofe(puebla, veracruz).
+limitrofe(quintana_roo, yucatan).
+limitrofe(san_luis_potosi, tamaulipas).
+limitrofe(san_luis_potosi, veracruz).
+limitrofe(san_luis_potosi, zacatecas).
+limitrofe(sinaloa, sonora).
+limitrofe(tabasco, veracruz).
+limitrofe(tamaulipas, veracruz).
+
+% Destinos Turisticos
+% desTur(Estado, Nombre)
+
+desTur(aguascalientes, feria_de_san_marcos).
+desTur(aguascalientes, centro_historico_aguascalientes).
+desTur(baja_california, ensenada).
+desTur(baja_california, rosarito).
+desTur(baja_california, la_bufadora).
+desTur(baja_california_sur, cabo_san_lucas).
+desTur(baja_california_sur, la_paz).
+desTur(baja_california_sur, playa_balandra).
+desTur(campeche, ciudad_amurallada_campeche).
+desTur(campeche, calakmul).
+desTur(chiapas, palenque).
+desTur(chiapas, canon_del_sumidero).
+desTur(chiapas, san_cristobal_de_las_casas).
+desTur(chiapas, cascadas_de_agua_azul).
+desTur(chihuahua, barrancas_del_cobre).
+desTur(chihuahua, el_chepe).
+desTur(chihuahua, dunas_de_samalayuca).
+desTur(cdmx, zocalo).
+desTur(cdmx, museo_de_antropologia).
+desTur(cdmx, coyoacan).
+desTur(cdmx, xochimilco).
+desTur(coahuila, cuatro_cienegas).
+desTur(coahuila, parras_de_la_fuente).
+desTur(coahuila, museo_del_desierto).
+desTur(colima, manzanillo).
+desTur(colima, comala).
+desTur(colima, volcan_de_fuego).
+desTur(durango, centro_historico_durango).
+desTur(durango, paseo_del_viejo_oeste).
+desTur(guanajuato, guanajuato_capital).
+desTur(guanajuato, san_miguel_de_allende).
+desTur(guanajuato, callejon_del_beso).
+desTur(guerrero, acapulco).
+desTur(guerrero, taxco).
+desTur(guerrero, ixtapa_zihuatanejo).
+desTur(hidalgo, grutas_de_tolantongo).
+desTur(hidalgo, real_del_monte).
+desTur(hidalgo, prismas_basalticos).
+desTur(jalisco, puerto_vallarta).
+desTur(jalisco, guadalajara).
+desTur(jalisco, tequila).
+desTur(jalisco, lago_de_chapala).
+desTur(mexico, piramides_de_teotihuacan).
+desTur(mexico, valle_de_bravo).
+desTur(mexico, nevado_de_toluca).
+desTur(michoacan, morelia).
+desTur(michoacan, patzcuaro).
+desTur(michoacan, santuario_mariposa_monarca).
+desTur(morelos, tepoztlan).
+desTur(morelos, cuernavaca).
+desTur(morelos, las_estacas).
+desTur(nayarit, sayulita).
+desTur(nayarit, punta_mita).
+desTur(nayarit, islas_marietas).
+desTur(nuevo_leon, parque_fundidora).
+desTur(nuevo_leon, cola_de_caballo).
+desTur(nuevo_leon, cerro_de_la_silla).
+desTur(oaxaca, oaxaca_de_juarez).
+desTur(oaxaca, monte_alban).
+desTur(oaxaca, hierve_el_agua).
+desTur(oaxaca, huatulco).
+desTur(puebla, centro_historico_puebla).
+desTur(puebla, piramide_de_cholula).
+desTur(puebla, cuetzalan).
+desTur(queretaro, santiago_de_queretaro).
+desTur(queretaro, pena_de_bernal).
+desTur(queretaro, region_vinos_tequisquiapan).
+desTur(quintana_roo, cancun).
+desTur(quintana_roo, tulum).
+desTur(quintana_roo, playa_del_carmen).
+desTur(quintana_roo, cozumel).
+desTur(quintana_roo, xcaret).
+desTur(san_luis_potosi, huasteca_potosina).
+desTur(san_luis_potosi, xilitla).
+desTur(san_luis_potosi, real_de_catorce).
+desTur(san_luis_potosi, sotano_de_las_golondrinas).
+desTur(sinaloa, mazatlan).
+desTur(sinaloa, el_fuerte).
+desTur(sinaloa, culiacan).
+desTur(sonora, san_carlos).
+desTur(sonora, puerto_penasco).
+desTur(sonora, desierto_de_altar).
+desTur(tabasco, parque_museo_la_venta).
+desTur(tabasco, comalcalco).
+desTur(tamaulipas, playa_miramar).
+desTur(tamaulipas, tula).
+desTur(tlaxcala, cacaxtla_xochitecatl).
+desTur(tlaxcala, huamantla).
+desTur(tlaxcala, santuario_luciernagas_nanacamilpa).
+desTur(veracruz, el_tajin).
+desTur(veracruz, puerto_de_veracruz).
+desTur(veracruz, catemaco).
+desTur(yucatan, chichen_itza).
+desTur(yucatan, merida).
+desTur(yucatan, uxmal).
+desTur(yucatan, cenote_ik_kil).
+desTur(zacatecas, centro_historico_zacatecas).
+desTur(zacatecas, cerro_de_la_bufa).
+desTur(zacatecas, mina_el_eden).
+
+% --- REGLAS ---
+
+% Capitales y Estados
+    % Si E es un Estado, sin importar su Extension TerritoriaL (A)
+    esEstado(E) :- estado(E, _).
+
+    % Si C es Capital de E y E es un estado
+    esCapital(C, E) :-
+        esEstado(E),
+        capital(E, C).
+    
+    %Estados Limitrofes
+    esLimitrofe(E1, E2) :- limitrofe(E1, E2); limitrofe(E2, E1).
+% Destinos Turisticos
+    % Si T es Destino Turistico de E y E es un estado
+    esTuristico(T, E) :-
+        esEstado(E),
+        desTur(E, T).
+
+% Tamanio de los Estados
+    % Si A de E es menor a 6000km2
+    esPequenio(E) :- 
+        estado(E, A),
+        A < 6000.
+
+    % Si A de E es mayor a 10000km2
+    esGrande(E) :-
+        estado(E, A),
+        A > 100000.
+
+    % A de E es comparada contra A de los demas estados hasta encontrar el caso donde A2 sea la mas grande
+    masGrande(E) :- 
+        estado(E, A), \+ (estado(_, A2), A2 > A).
+    
+    % A de E es comparada contra A de los demas estados hasta encontrar el caso donde A2 sea la mas pequenia
+    masPequenio(E) :- 
+        estado(E, A), \+ (estado(_, A2), A2 < A).
 
 distCapitales().
 
-desTuristico().
+esSuperficie().
